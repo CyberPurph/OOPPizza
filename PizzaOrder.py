af7e4cc04e8a5a34fa64de76ad2290615dc3d19c
xWIREHEADx <mattadamosart@gmail.com>
2023-10-19 23:36:42 -0400

    Name Changes

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -9,20 +9,20 @@ class User:
         return f"^^USER^^ \nName - {self.name} \nEmail - {self.email} \nAddress - {self.address}"
     
 class Pizza:
-    def __init__(self, size, price, num):
+    def __init__(self, size, price, quantity):
         self.size = size
         self.price = price
-        self.num = num
+        self.quantity = quantity
 
     def total(self):
-        if self.num >= 3:
-            total = self.num * self.price * 0.85
+        if self.quantity >= 3:
+            total = self.quantity * self.price * 0.85
         else:
-            total = self.num * self.price
+            total = self.quantity * self.price
         return total
 
     def __str__(self):
-        return f"^^PIIZAS^^ \nSize - {self.size} \nPrice per Pizza - {self.price} \nQuantity - {self.num}"
+        return f"^^PIIZAS^^ \nSize - {self.size} \nPrice per Pizza - {self.price} \nQuantity - {self.quantity}"
     
 #Input
 user_name = input("Who's ordering? ")
@@ -39,11 +39,11 @@ pizza_price = {
 if pizza_size not in pizza_price:
     print("Please enter pizza size as shown. Try again.")
 
-pizza_num = int(input("Enter the amount of pizzas you want "))
+pizza_quantity = int(input("Enter the amount of pizzas you want "))
 
 #Objects
 user = User(user_name, user_email, user_address)
-pizza = Pizza(pizza_size, pizza_price[pizza_size], pizza_num)
+pizza = Pizza(pizza_size, pizza_price[pizza_size], pizza_quantity)
 
 #Total Price
 
